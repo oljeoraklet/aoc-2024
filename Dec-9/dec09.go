1b73e1e4f8fd15bed4d3c1b99f9320cb9319a8b9
Olle Wikström <olle.wikstrom@sogeti.se>
2024-12-09 18:12:50 +0100

    Part two done

--- Dec-9/dec09.go
+++ Dec-9/dec09.go
@@ -7,13 +7,117 @@ import (
 	"strconv"
 )
 
+type DiskSpace struct {
+	Position int
+	Length   int
+}
+
 func main() {
+
+	// partOne()
+	partTwo()
+
+}
+
+func partTwo() {
 	file, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer file.Close()
 
+	numbers := []int{}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanRunes)
+	for scanner.Scan() {
+		number, _ := strconv.Atoi(scanner.Text())
+		numbers = append(numbers, number)
+
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+
+	files := make(map[int]DiskSpace)
+	freeSpaces := []DiskSpace{}
+
+	currFileIndex := 0
+	currPosition := 0
+
+	for i, number := range numbers {
+		if i%2 == 0 {
+			//Save the file position and length under the file index
+			files[currFileIndex] = DiskSpace{Position: currPosition, Length: number}
+			currFileIndex++
+		} else {
+			freeSpaces = append(freeSpaces, DiskSpace{Position: currPosition, Length: number})
+		}
+		//Increase the current position by the number
+		currPosition += number
+	}
+
+	fmt.Println("Files: ", files)
+	fmt.Println("Free spaces: ", freeSpaces)
+
+	for currFileIndex > 1 {
+		currFileIndex--
+		file := files[currFileIndex]
+
+		for i, freeSpace := range freeSpaces {
+			//If the free space is after the file, we don't need to consider it
+			if freeSpace.Position >= file.Position {
+				break
+			}
+			if file.Length <= freeSpace.Length {
+				//If the file fits, move the position to the free space start
+				files[currFileIndex] = DiskSpace{Position: freeSpace.Position, Length: file.Length}
+
+				if file.Length == freeSpace.Length {
+					//If the free space and file are the same length, the gap no longer exists
+					freeSpaces = remove(freeSpaces, i)
+				} else {
+					freeSpaces[i] = DiskSpace{Position: freeSpace.Position + file.Length, Length: freeSpace.Length - file.Length}
+				}
+				break
+			}
+		}
+	}
+
+	fmt.Println("Files: ", files)
+
+	checkSum := getChecksumTwo(files)
+
+	fmt.Println("Checksum: ", checkSum)
+
+}
+
+func remove(slice []DiskSpace, s int) []DiskSpace {
+	return append(slice[:s], slice[s+1:]...)
+}
+
+func getChecksumTwo(files map[int]DiskSpace) int {
+	checkSum := 0
+
+	for key, file := range files {
+
+		fmt.Printf("Key: %d, File: %v\n", key, file)
+
+		for j := file.Position; j < file.Position+file.Length; j++ {
+			checkSum += key * j
+		}
+	}
+
+	return checkSum
+}
+
+func partOne() {
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
 	numbers := []int{}
 
 	currDataIndex := 0
@@ -49,30 +153,48 @@ func main() {
 	fmt.Println("Disk data: ", compactedData)
 
 	fmt.Println("Compacted string: ", compact(compactedData))
-	fmt.Println("Checksum: ", getChecksum(compact(compactedData)))
-
+	fixedData := compact(compactedData)
+	fmt.Println("Checksum: ", getChecksum(fixedData))
 }
 
-func compact(runes []int) []int {
-	lastIndex := len(runes) - 1
+func compact(numbers []int) []int {
+	lastIndex := len(numbers) - 1
 
-	fmt.Println("Last element: ", runes[lastIndex])
+	for i := 0; i < lastIndex; i++ {
+		if numbers[i] == -1 {
+			for numbers[lastIndex] == -1 && lastIndex > i {
+				lastIndex--
+			}
+			if lastIndex <= i {
+				return numbers[:lastIndex]
+			}
+			numbers[i], numbers[lastIndex] = numbers[lastIndex], numbers[i]
+		}
+	}
+
+	return numbers
+}
+
+func compactTwo(numbers []int) []int {
+	lastIndex := len(numbers) - 1
+
+	fmt.Println("Last element: ", numbers[lastIndex])
 
 	for i := 0; i < lastIndex; i++ {
 		fmt.Println("Index: ", i)
-		if runes[i] == -1 {
-			for runes[lastIndex] == -1 && lastIndex > i {
+		if numbers[i] == -1 {
+			for numbers[lastIndex] == -1 && lastIndex > i {
 				lastIndex--
 			}
 			if lastIndex <= i {
 				fmt.Println("Break")
-				return runes[:lastIndex]
+				return numbers[:lastIndex]
 			}
-			runes[i], runes[lastIndex] = runes[lastIndex], runes[i]
+			numbers[i], numbers[lastIndex] = numbers[lastIndex], numbers[i]
 		}
 	}
 
-	return runes
+	return numbers
 }
 
 func getChecksum(runes []int) int {
